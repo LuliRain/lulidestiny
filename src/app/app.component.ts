fd9e753bbf6a8c51fff763c292099de2e31c8de8
Luli Rain <cutelilithx@gmail.com>
2025-02-21 17:08:59 -0300

    hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
-
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +11,15 @@ import { MenuComponent } from './menu/menu.component';
 })
 export class AppComponent {
   title = 'Luli';
+  
+  firebaseConfig = {
+    apiKey: "AIzaSyCkIc9K-4Zsoc_xKQ1w6Eqorvcyuk45Fng",
+    authDomain: "luligame.firebaseapp.com",
+    projectId: "luligame",
+    storageBucket: "luligame.firebasestorage.app",
+    messagingSenderId: "75419793705",
+    appId: "1:75419793705:web:756fb268512e4cac269f38"
+  };
+
+  app = initializeApp(this.firebaseConfig);
 }
